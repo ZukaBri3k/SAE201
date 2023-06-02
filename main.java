75e33816b265ca2bdad3026fd737a2c1cf5af35a
EwanP56 <120094397+EwanP56@users.noreply.github.com>
2023-06-02 15:32:39 +0200

    first commit : début de l'IHM

---
+++ main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application{
+	
+	public void start(Stage maFenetre){
+		maFenetre = new Recap();
+		maFenetre.show();
+	}
+		
+
+	public static void main(String[] args) {
+		Application.launch();
+
+	}
+
+}
