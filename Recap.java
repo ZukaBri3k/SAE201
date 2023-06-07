f37e4a84df959b4364346a7b584585ab16a521ee
EwanP56 <120094397+EwanP56@users.noreply.github.com>
2023-06-07 09:43:39 +0200

    Update Recap.java et fin

--- Recap.java
+++ Recap.java
@@ -11,6 +11,7 @@ import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -18,19 +19,34 @@ import javafx.util.Callback;
 public class Recap extends Stage{
 	private GridPane root = new GridPane();
 	
+	private VBox num_chambre = new VBox();
+	
 	private Label lblnumReserv = new Label("Numéro de reservation :");
 	private Label lblReserv = new Label();
 	
+	private Label lblNom = new Label("Nom :");
+	private Label lblnom = new Label();
+	private Label lblPrenom = new Label("Prenom :");
+	private Label lblpren = new Label();
+	private Label lblNum = new Label("Numéro de téléphone :");
+	private Label lblNumTel = new Label();
+	private Label lblNb = new Label("Nombre de personne :");
+	private Label lblNbPer = new Label();
+	
+	private Label lblNumCh = new Label("Numéro de(s) chambre(s) :");
+	private Label lblNuCh = new Label();
 	private Label lblnbCh = new Label("Nombre de chambre :");
 	private Label lblCh = new Label();
 	
 	private Label lbldateD = new Label("Date :");
 
 	private DatePicker dateDebut = new DatePicker();
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+	private LocalDate dateD;
+	private LocalDate dateF;
 	private DatePicker dateFin = new DatePicker();
 	
 	private Label lblChLib = new Label("Chambre non libre");
+	private boolean estLibre;
 	
 	private Label lblnumPla = new Label("Nombre de place :");
 	private Label lblPlace = new Label();
@@ -38,15 +54,45 @@ public class Recap extends Stage{
 	private Label lblprix = new Label("Prix :");
 	private Label lblprice = new Label();
 	
-	private Label lblperArr = new Label("Personne pas encore arrivée");
-	
 	private Button bModifier = new Button("Modifier");
 	private Button bFermer = new Button("Fermer");
 	private Button bEnvoyer = new Button("Valider");
 	
-	public Recap(){
+	
+	public Recap(Reservation reserv){
+		boolean paslibre = true;
+		this.lblReserv.setText(String.valueOf(reserv.getNumero_reservation()));
+		
+		this.lblnom.setText(reserv.getReserve().getNom());
+		this.lblpren.setText(reserv.getReserve().getPrenom());
+		this.lblNumTel.setText(reserv.getReserve().getNumero_tel());
+		this.lblNbPer.setText(String.valueOf(reserv.getNb_personne()));
+		
+		int total = 0;
+		int prixto = 0;
+		for (Chambre c : reserv.getListe_chambre()) {
+			this.lblNuCh.setText(String.valueOf(c.getNumChambre()));
+			this.num_chambre.getChildren().addAll(this.lblNuCh);
+			if(c.isEstLibre() == false) {
+				paslibre = false;
+			}
+			total += c.getNbPlace();
+			this.lblPlace.setText(String.valueOf(total));
+			prixto += c.getPrix();
+			this.lblprice.setText(String.valueOf(prixto));
+		}
+		
+		this.estLibre = paslibre;
+		
+		this.lblCh.setText(String.valueOf(reserv.getNb_chambre()));
+		
+		this.dateD = reserv.getDate_debut();
+		this.dateF = reserv.getDate_fin();
+		this.dateDebut.setValue(dateD);
+		this.dateFin.setValue(dateF);
+		
 		this.setTitle("Récapitulatif de la réservation");
-		this.setResizable(true);
+		this.setResizable(false);
 		this.sizeToScene();
 		
 		bFermer.setOnAction(e -> this.close());
@@ -55,22 +101,6 @@ public class Recap extends Stage{
 	}
 	
 	private Parent creerContenu() {
-		boolean estLibre = false;
-		boolean estArrive = true;
-		
-		String reserv = "1";
-		lblReserv.setText(reserv);
-		
-		String chambre = "1";
-		lblCh.setText(chambre);
-		
-		String dd = "24/01/2004";
-	    LocalDate dateD = LocalDate.parse(dd, formatter);
-		dateDebut.setValue(dateD);
-		String df = "05/02/2004";
-		LocalDate dateF = LocalDate.parse(df, formatter);
-		dateFin.setValue(dateF);
-		
 		dateDebut.setShowWeekNumbers(false);
 		
 		final Callback<DatePicker, DateCell> dayCellFactory =
@@ -102,33 +132,25 @@ public class Recap extends Stage{
 		
 		lblChLib.setTextFill(Color.RED);
 		
-		String place = "4";
-		lblPlace.setText(place);
-		
-		String price = "400€";
-		lblprice.setText(price);
-		
-		lblperArr.setTextFill(Color.RED);
-		
 		root.addRow(0, lblnumReserv, lblReserv);
-		root.addRow(1, lblnbCh, lblCh);
-		root.addRow(2, lbldateD, dateDebut);
-		root.addRow(3, lblChLib);
+		root.addRow(1, lblNom, lblnom);
+		root.addRow(2, lblPrenom, lblpren);
+		root.addRow(3, lblNum, lblNumTel);
+		root.addRow(4, lblNb, lblNbPer);
+		root.addRow(5, lblNumCh, num_chambre);
+		root.addRow(6, lblnbCh, lblCh);
+		root.addRow(7, lbldateD, dateDebut);
+		root.addRow(8, lblChLib);
 		if(estLibre == true) {
 			lblChLib.setText("Chambre libre");
 			lblChLib.setTextFill(Color.GREEN);
 		}
-		root.addRow(4, lblnumPla, lblPlace);
-		root.addRow(5, lblprix, lblprice);
-		root.addRow(6, lblperArr);
-		if(estArrive == true) {
-			lblperArr.setText("Personne arrivée");
-			lblperArr.setTextFill(Color.GREEN);
-		}
+		root.addRow(9, lblnumPla, lblPlace);
+		root.addRow(10, lblprix, lblprice);
 		
-		root.add(bModifier, 0, 7);
-		root.add(bFermer, 1, 7);
-		root.add(bEnvoyer, 1, 7);
+		root.add(bModifier, 0, 11);
+		root.add(bFermer, 1, 11);
+		root.add(bEnvoyer, 1, 11);
 		
 		GridPane.setHalignment(bFermer, HPos.RIGHT);
         GridPane.setValignment(bFermer, VPos.CENTER);
