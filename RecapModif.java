b1121939449062e100b015893ce263b88bbd0952
EwanP56 <120094397+EwanP56@users.noreply.github.com>
2023-06-09 08:40:17 +0200

    VRAI fin RecapModif.java

--- RecapModif.java
+++ RecapModif.java
@@ -20,6 +20,7 @@ import javafx.stage.Stage;
 
 public class RecapModif extends Stage{
 	private GridPane root = new GridPane();
+	//Les HBox utile pour aligner les informations si il y'a plusieurs chambre
 	private HBox num_chambre = new HBox();
 	private HBox hboxprix = new HBox();
 	private HBox hboxplace = new HBox();
@@ -37,7 +38,7 @@ public class RecapModif extends Stage{
 	private TextField txtNbPer = new TextField();
 	
 	private Label lblNumCh = new Label("Numéro de(s) chambre(s) :");
-	private TextField txtNuCh = new TextField();
+	private TextField txtnumChambre= new TextField();
 	private Label lblnbCh = new Label("Nombre de chambre :");
 	private TextField txtCh = new TextField();
 	
@@ -53,49 +54,53 @@ public class RecapModif extends Stage{
 	private boolean estLibre;
 	
 	private Label lblnumPla = new Label("Nombre de place :");
-	private TextField txtPlace = new TextField();
+	private TextField txtPlace= new TextField();
 	
 	private Label lblprix = new Label("Prix :");
-	private TextField txtprice = new TextField();
+	private TextField txtprice= new TextField();
 	
 	private Button bFermer = new Button("Fermer");
 	private Button bEnvoyer = new Button("Valider");
 	
+	//combobox pour le choix de liste déroulante
 	private ComboBox<String> cbChambre = new ComboBox<String>();
 	
 	public RecapModif(Reservation reserv){
 		boolean paslibre = true;
+		//initialisation des textes avec les informations de la reservation
 		this.txtReserv.setText(String.valueOf(reserv.getNumero_reservation()));
 		this.txtnom.setText(reserv.getReserve().getNom());
 		this.txtpren.setText(reserv.getReserve().getPrenom());
 		this.txtNumTel.setText(reserv.getReserve().getNumero_tel());
 		this.txtNbPer.setText(String.valueOf(reserv.getNb_personne()));
-		
+		txtnumChambre.setText(String.valueOf(reserv.getListe_chambre().get(0).getNumChambre()));
+		txtPlace.setText(String.valueOf(reserv.getListe_chambre().get(0).getNbPlace()));
+		txtprice.setText(String.valueOf(reserv.getListe_chambre().get(0).getPrix()));
+		//parcours de tableau pour récupérer les informations de la liste de chambre de la réservation
 		for (int i = 0; i < reserv.getListe_chambre().size(); i++) {
 		    Chambre c = reserv.getListe_chambre().get(i);
-		    this.num_chambre.getChildren().add(new TextField(String.valueOf(c.getNumChambre())));
-		    
-
+		    //le if ici sert à savoir si une chambre dans la liste est pas libre alors la reservation n'est pas libre du tout
 		    if (c.isEstLibre() == false) {
 		        paslibre = false;
 		    }
-			this.hboxplace.getChildren().add(new TextField(String.valueOf(c.getNbPlace())));
-			this.hboxprix.getChildren().add(new TextField(String.valueOf(c.getPrix())));
-			if (i < reserv.getListe_chambre().size() - 1) {
-		        this.num_chambre.getChildren().add(new Label(";"));
-		        this.hboxplace.getChildren().add(new Label(";"));
-		        this.hboxprix.getChildren().add(new Label(";"));
+			//les séparateurs avec un ";" entre chaque données 
+			if (i >= 1) {
+				txtnumChambre.setText(txtnumChambre.getText()+";"+c.getNumChambre());
+				txtPlace.setText(txtPlace.getText()+";"+c.getNbPlace());
+		        txtprice.setText(txtprice.getText()+";"+c.getPrix());
 		    }
 		}
+		//hbox pour aligner les informations qui seront à modifier
+		this.hboxplace.getChildren().add(txtPlace);
+		this.hboxprix.getChildren().add(txtprice);
+		this.num_chambre.getChildren().add(txtnumChambre);
 		this.estLibre = paslibre;
 		
 		this.txtCh.setText(String.valueOf(reserv.getNb_chambre()));
-		
 		this.dateD = reserv.getDate_debut();
 		this.dateF = reserv.getDate_fin();
 		this.dateDebut.setValue(dateD);
 		this.dateFin.setValue(dateF);
-		
 		cbChambre.getItems().addAll(
 				"Libre", 
 				"Pas libre"
@@ -121,11 +126,12 @@ public class RecapModif extends Stage{
 	}
 	
 	private Parent creerContenu() {
-		
+		//pour retirer le numéro des semaine sur le calendrier
 		dateDebut.setShowWeekNumbers(false);
 		
 		lblChLib.setTextFill(Color.RED);
 		
+		//ajout des libelle et des textes ainsi que des hbox dans le gridpane
 		root.addRow(0, lblnumReserv, txtReserv);
 		root.addRow(1, lblNom, txtnom);
 		root.addRow(2, lblPrenom, txtpren);
